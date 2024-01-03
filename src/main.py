000109589f2f68fbf6c1a97d80f926ef6a176fb4
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2024-01-02 22:03:24 -0500

    gui fonts and styling

--- src/main.py
+++ src/main.py
@@ -1,34 +1,42 @@
 import tkinter as tk
+from tkinter import font
 import threading
 import queue
 from chord import make_chord_handler, stream
-
+"""arial rounded mt bold gm7#
+yuppy sc gm7#
+yugothic
+Baloo 2
+"""
 class App(tk.Tk):
-    def __init__(self) -> None:
+    def __init__(self, tick=500) -> None:
         super().__init__()
+        self.tick = tick
+        
         self.title("Chordy")
         self.geometry('700x350')
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.frame = tk.Frame(self, background="white")
+        self.frame = tk.Frame(self, background="black")
         self.frame.grid(row=0, column=0, sticky="ewns")
-        self.frame.grid_rowconfigure(0, weight=1)
-        self.frame.grid_columnconfigure(0, weight=1)
 
-        self.info_frame = tk.Frame(self.frame, background="purple", width=100, height=50)
+        self.frame.grid_anchor("center")
+
+        self.info_frame = tk.Frame(self.frame, background="black")
         self.info_frame.grid(row=0, column=0, sticky="")
+        self.info_frame.grid_propagate(True)
 
-        self.chord_var = tk.StringVar()        
-        self.chord_label = tk.Label(self.info_frame, textvariable=self.chord_var)
-        self.chord_label.pack()
+        self.default_font = "Arial Rounded Mt Bold"
+        self.chord_var = tk.StringVar(value="None")        
+        self.chord_label = tk.Label(self.info_frame, fg="white", bg="black", padx=5, font=(self.default_font, 36), textvariable=self.chord_var)
+        self.chord_label.grid(row=0, column=0, sticky="ew")
 
-        self.prob_var = tk.StringVar()
-        self.prob_label = tk.Label(self.info_frame, textvariable=self.prob_var)
-        self.prob_label.pack()
+        self.prob_var = tk.StringVar(value="0.00%")
+        self.prob_label = tk.Label(self.info_frame, fg="white", bg="black", padx=5, font=(self.default_font, 12), textvariable=self.prob_var)
+        self.prob_label.grid(row=1, column=0, sticky="ew")
 
         # self.protocol('WM_DELETE_WINDOW', lambda: )
-
         
         self.main()
     
@@ -47,11 +55,14 @@ class App(tk.Tk):
                     data = q.get()
                 
                 chord, prob = data
+                
+                if chord is None: chord = "○"
                 if prob is None: prob = 0
+
                 self.chord_var.set(chord)
                 self.prob_var.set("{:.2f}%".format(prob*100))
 
-            self.after(1000, poll)
+            self.after(self.tick, poll)
         
         poll()
         
