ea07306a9047fa00db168a180d73605a39818691
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2024-01-02 19:31:38 -0500

    tkinter + multithreading / queueing stream data

---
+++ src/chord.py
@@ -0,0 +1,93 @@
+from enum import IntEnum
+from collections import Counter
+from pyACA import computeChords, computeChordsCl
+
+import sys
+import numpy as np
+import pyaudio
+import wave
+from scipy.io.wavfile import read as wavread
+
+def norm_audio(x):
+    if x.dtype == 'float32':
+        audio = x
+    else:
+        # change range to [-1,1)
+        if x.dtype == 'uint8':
+            nbits = 8
+        elif x.dtype == 'int16':
+            nbits = 16
+        elif x.dtype == 'int32':
+            nbits = 32
+
+        audio = x / float(2**(nbits - 1))
+
+    # special case of unsigned format
+    if x.dtype == 'uint8':
+        audio = audio - 1.
+
+    return audio
+
+def stream(handler=lambda *args, **kwargs : None, max_frames=1e99, chunk=1024, channels=1, fs=44100):
+    p = pyaudio.PyAudio()
+    print("initialized pyaudio")
+
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+    print("opened stream")
+
+    frames = np.array([])
+
+    try:
+        while True:
+            bdata = stream.read(chunk)
+            data = norm_audio(np.frombuffer(bdata, dtype=np.int16))
+
+            frames = np.append(frames, data)
+            if frames.shape[0] >= max_frames:
+                frames = frames[-max_frames:]
+
+            handler(frames, fs=fs)
+            
+    except KeyboardInterrupt: 
+        pass # continue to close the stream
+
+    print("close stream")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+class ChordAlgo(IntEnum):
+    RAW = 0
+    VITERBI = 1
+
+def make_chord_handler(callback=lambda *args, **kwargs : None, threshold=0.05, algorithm=ChordAlgo.RAW, iBlockLength=8192, iHopLength=2048):
+    def handler(frames, *args, **kwargs):
+        fs = kwargs['fs']
+        
+        cChordLabel, aiChordIdx, t, P_E = computeChords(frames, fs, iBlockLength=iBlockLength, iHopLength=iHopLength)
+        chordLabel, chordIdx = cChordLabel[algorithm], aiChordIdx[algorithm]
+
+        chordLabel = np.array(chordLabel)
+
+        chordDict = {}
+        for i,chord in enumerate(chordLabel):
+            if chord not in chordDict: chordDict[chord] = 0
+            chordDict[chord] += P_E[chordIdx[i]][0]   
+        mostLikelyChord = list(max(chordDict.items(), key=lambda x: x[1]))
+        mostLikelyChord[1] /= len(chordLabel) # max avg probability of chord over frames
+
+        chord, prob = mostLikelyChord
+        chord = formatChordLabel(chord)
+        if prob < threshold:
+            chord = None
+        
+        callback(chord, prob)
+        
+    return handler
+
+def formatChordLabel(label: str):
+    return label.upper().replace("MAJ", "").replace("MIN", "m").replace(" ", "")
--- src/main.py
+++ src/main.py
@@ -1,91 +1,75 @@
-from enum import Enum
-from collections import Counter
-from pyACA import computeChords, computeChordsCl
+import tkinter as tk
+import threading
+import queue
+from chord import make_chord_handler, stream
 
-import sys
-import numpy as np
-import pyaudio
-import wave
-from scipy.io.wavfile import read as wavread
+class App(tk.Tk):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title("Chordy")
+        self.geometry('700x350')
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-def norm_audio(x):
-    if x.dtype == 'float32':
-        audio = x
-    else:
-        # change range to [-1,1)
-        if x.dtype == 'uint8':
-            nbits = 8
-        elif x.dtype == 'int16':
-            nbits = 16
-        elif x.dtype == 'int32':
-            nbits = 32
+        self.frame = tk.Frame(self, background="white")
+        self.frame.grid(row=0, column=0, sticky="ewns")
+        self.frame.grid_rowconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(0, weight=1)
 
-        audio = x / float(2**(nbits - 1))
+        self.info_frame = tk.Frame(self.frame, background="purple", width=100, height=50)
+        self.info_frame.grid(row=0, column=0, sticky="")
 
-    # special case of unsigned format
-    if x.dtype == 'uint8':
-        audio = audio - 1.
+        self.chord_var = tk.StringVar()        
+        self.chord_label = tk.Label(self.info_frame, textvariable=self.chord_var)
+        self.chord_label.pack()
 
-    return audio
+        self.prob_var = tk.StringVar()
+        self.prob_label = tk.Label(self.info_frame, textvariable=self.prob_var)
+        self.prob_label.pack()
 
-def stream(handler, max_frames=1e99, chunk=1024, channels=1, fs=44100):
-    p = pyaudio.PyAudio()
-    print("initialized pyaudio")
+        # self.protocol('WM_DELETE_WINDOW', lambda: )
 
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=channels,
-                    rate=fs,
-                    frames_per_buffer=chunk,
-                    input=True)
-    print("opened stream")
-
-    frames = np.array([])
-
-    try:
-        while True:
-            bdata = stream.read(chunk)
-            data = norm_audio(np.frombuffer(bdata, dtype=np.int16))
-
-            frames = np.append(frames, data)
-            if frames.shape[0] >= max_frames:
-                frames = frames[-max_frames:]
-
-            handler(frames, fs=fs)
-            
-    except KeyboardInterrupt: 
-        pass # continue to close the stream
-
-    print("close stream")
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-class ChordAlgo(Enum):
-    RAW = 1
-    VITERBI = 2
-
-def make_chord_handler(algorithm=ChordAlgo.RAW, iBlockLength=8192, iHopLength=2048):
-    def handler(frames, *args, **kwargs):
-        fs = kwargs['fs']
         
-        (rawChordLabel, viterbiChordLabel), aiChordIdx, t, P_E = computeChords(frames, fs, iBlockLength=iBlockLength, iHopLength=iHopLength)
+        self.main()
+    
+    def main(self):
+        q = queue.Queue()
+        thread = threading.Thread(target=self.start_stream, args=(q,))
+        thread.daemon = True
+        thread.start()
         
-        chordLabel = None
-        if algorithm == ChordAlgo.RAW: 
-            chordLabel = rawChordLabel
-        elif algorithm == ChordAlgo.VITERBI:
-            chordLabel = viterbiChordLabel
-        
-        if chordLabel is None:
-            raise Exception("Algorithm type doesn't exist")
-        
-        chordLabel = np.array(chordLabel)
-        print(chordLabel)
-        
-    return handler
+        def poll():
+            nonlocal q
 
-if __name__ == "__main__":
-    HOP_MEMORY = 4
-    HOP_LENGTH = 2048
-    handler = make_chord_handler(iHopLength=HOP_LENGTH)
-    stream(handler, max_frames=HOP_LENGTH*HOP_MEMORY)
+            if not q.empty():
+                data = None # get last queued message and clear queue
+                while not q.empty():
+                    data = q.get()
+                
+                chord, prob = data
+                if prob is None: prob = 0
+                self.chord_var.set(chord)
+                self.prob_var.set("{:.2f}%".format(prob*100))
+
+            self.after(1000, poll)
+        
+        poll()
+        
+
+    def start_stream(self, q:queue.Queue):
+        HOP_MEMORY = 4
+        HOP_LENGTH = 2048
+        
+        def callback(chord: str|None, prob: float):
+            if chord is None: prob = None
+            q.put([chord, prob])
+
+        handler = make_chord_handler(callback, iHopLength=HOP_LENGTH)
+        stream(handler, max_frames=HOP_LENGTH*HOP_MEMORY)
+        
+    
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
+
+
